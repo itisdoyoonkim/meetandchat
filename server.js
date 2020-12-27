115099b12eeac3351042549a9c1b7b5f6dd887a0
Steve Kim <dkim.steve@gmail.com>
2020-12-26 22:13:51 -0800

    added express as dependency

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const app = express();
-var cors = require("cors");
-const port = process.env.PORT || 5000;
+const cors = require("cors");
 const path = require("path");
 const axios = require("axios");
+const port = process.env.PORT || 5000;
 const fs = require("fs");
 
 app.use(cors());
