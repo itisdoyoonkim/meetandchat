bd21544694964b0d01c917743d74bdf5ceb27b50
Steve Kim <dkim.steve@gmail.com>
2020-12-26 22:20:55 -0800

    fixed the location for serving static file

--- server.js
+++ server.js
@@ -11,9 +11,9 @@ app.use(cors());
 console.log(path.resolve(__dirname, "/build"));
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("/build"));
+  app.use(express.static("frontend/build"));
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "/build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
   });
 }
 
