6fb3dd9be3e8e2abfda48af429627a88398f9020
Steve Kim <dkim.steve@gmail.com>
2020-12-26 22:43:50 -0800

    test by removing production env check

--- server.js
+++ server.js
@@ -10,12 +10,12 @@ app.use(cors());
 
 console.log(path.resolve(__dirname, "/build"));
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("frontend/build"));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
-  });
-}
+// if (process.env.NODE_ENV === "production") {
+app.use(express.static("frontend/build"));
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+});
+// }
 
 app.get("/getLinks", (req, res) => {
   const API =
