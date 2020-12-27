d85d40a5f2437a4ba879e809a6ab20f4ffbb8c74
Steve Kim <dkim.steve@gmail.com>
2020-12-26 23:41:13 -0800

    changed * to /

--- server.js
+++ server.js
@@ -8,14 +8,15 @@ const fs = require("fs");
 
 app.use(cors());
 
-console.log(path.resolve(__dirname, "/build"));
+console.log(path.join(__dirname, "frontend/build"));
 
-// if (process.env.NODE_ENV === "production") {
-app.use(express.static("frontend/build"));
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
-});
-// }
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "frontend/build")));
+
+  app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend/build", "index.html"));
+  });
+}
 
 app.get("/getLinks", (req, res) => {
   const API =
